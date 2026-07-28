4c7f893249c1925340642525820ce82650d5cd31
sooryanshh <sooryansh4578@gmail.com>
2026-07-28 13:48:12 +0530

    Time: 0 ms (100%), Space: 44.6 MB (65.22%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,20 +1,21 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<String>();
-        // StringBuilder sb = new StringBuilder();
-        String sb ="";
+        StringBuilder sb = new StringBuilder();
+       
         fun(ans,sb,0,0,n);
         return ans;
-
     }
-    void fun(List<String> ans , String sb, int co, int cc, int n ){
+    void fun(List<String> ans , StringBuilder sb, int co, int cc, int n ){
          if(co==n && cc==n ){
             ans.add(sb.toString());
             return;
         }
         if(co>n || cc>n || cc > co)return;
-       
-        fun(ans,sb+'(',co+1,cc,n);
-        fun(ans,sb+')',co,cc+1,n);
+        
+        fun(ans,sb.append('('),co+1,cc,n);
+        sb.deleteCharAt(sb.length()-1);
+        fun(ans,sb.append(')'),co,cc+1,n);
+        sb.deleteCharAt(sb.length()-1);
     }
 }
