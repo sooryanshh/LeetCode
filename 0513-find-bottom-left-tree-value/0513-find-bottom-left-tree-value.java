21b755bdaed5f2e311631a6eadd69e7c173a6318
sooryanshh <sooryansh4578@gmail.com>
2026-08-21 11:25:46 +0530

    Time: 4 ms (17.51%), Space: 46.4 MB (65.95%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -1,24 +1,24 @@
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         levelOrder(root,list);
-        return list.get(list.size()-1).get(0);
+        return list.get(list.size()-1);
     }
-     public void levelOrder(TreeNode root,List<List<Integer>> list) {
+     public void levelOrder(TreeNode root,List<Integer> list) {
         
         if(root== null)return;
         Queue<TreeNode> q = new ArrayDeque<>();
         q.add(root);
         while(!q.isEmpty()){
-            int n = q.size();
-            List<Integer> tempL = new ArrayList<>();
-            for(int i =0;i<n;i++){
+    
                 TreeNode temp = q.poll();
+                 if(root!=null) list.add(temp.val);
+                 if(temp.right != null )q.add(temp.right);
                 if(temp.left!=null)q.add(temp.left);
-                if(temp.right != null )q.add(temp.right);
-                tempL.add(temp.val);
-            }
-            list.add(tempL);
+               
+               
+            
+            
         }
         
     }
