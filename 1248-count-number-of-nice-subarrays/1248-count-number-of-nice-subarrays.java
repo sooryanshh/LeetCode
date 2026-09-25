539107a5cfbfc4c968d75e013ea2d20395699ab8
sooryanshh <sooryansh4578@gmail.com>
2026-09-25 14:08:08 +0530

    Time: 12 ms (71.72%), Space: 60 MB (7.72%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,24 +1,22 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int l =0;
-        int ans = 0;
-        int count = 0;
         Queue<Integer> q = new ArrayDeque<>();
-        int last =-1;
-        for(int i=0;i<nums.length;i++){
+        int l =0;
+        int ans =0,count =0;
+        for(int i =0;i<nums.length;i++){
             if(nums[i]%2!=0){
                 count++;
-                if(last==-1)last =i;
-                else q.add(i);
-                }
-            while(count>k){
-                if(nums[l]%2!=0){
-                    count--;
-                    last =q.poll();
-                    }
-                l++;
+                q.add(i);
             }
-            if(count ==k)ans+=last-l+1;
+           
+            if(count>k){
+                l = q.poll()+1;
+                count--;
+            }
+            if(count ==k){
+                
+                ans+=q.peek()-l+1;
+            }
         }
         return ans;
     }
