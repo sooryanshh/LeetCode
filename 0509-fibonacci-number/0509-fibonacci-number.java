746844096634bb9a86ae8fd1c8299ff63115ff8b
sooryanshh <sooryansh4578@gmail.com>
2026-09-25 15:16:45 +0530

    Time: 0 ms (100%), Space: 41.8 MB (85.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,13 @@
 class Solution {
+    private int fibo(int n ,int[] dp){
+        if(n<2)return n;
+        if(dp[n]!=0)return dp[n];
+        int ans =fibo(n-1,dp)+fibo(n-2,dp);
+        dp[n]= ans;
+        return ans;
+    }
     public int fib(int n) {
-        if(n==0)return 0;
-        if(n==1)return 1;
-        return fib(n-1)+fib(n-2);
+       // this is dp solution for fibonacci 
+        return fibo(n,new int[n+1]);
     }
 }
